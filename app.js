964923156b6f800ca7c49fe4e871fec66e6f722c
Bishal <bishal@192-168-1-104.tpgi.com.au>
2020-08-29 19:21:16 +1000

    deploying

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;;
+const port = process.env.PORT || 3000;
 const urlShortener = require('node-url-shortener');
 
 app.get('/', function(req, res) {
